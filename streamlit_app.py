070ec5f856a16578d6cc0c6ef7270bfeb8fad9f3
hudaykulovf <hudaykulov26@gmail.com>
2025-05-12 00:39:57 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -137,8 +137,9 @@ with col2:
     cbar = plt.colorbar(sm, ax=ax, fraction=0.046, pad=0.04)
     cbar.set_label('Relevance to Prediction', rotation=270, labelpad=15)
     st.pyplot(fig)
-        else:
-            st.markdown(f"- {name} — {conf:.2f}%")
+    
+else:
+    st.markdown(f"- {name} — {conf:.2f}%")
 
 # SIDEBAR
 with st.sidebar:
